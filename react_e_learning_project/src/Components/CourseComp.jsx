f725f85ad5abc89c05c9cc593ac1d1ad8c6e6f18
“3EnasMelad” <https://github.com/3EnasMelad>
2024-08-11 22:28:52 +0300

    add wishlist dialog

--- react_e_learning_project/src/Components/CourseComp.jsx
+++ react_e_learning_project/src/Components/CourseComp.jsx
@@ -1,125 +1,171 @@
-import {Button, Card, CardContent, CardMedia, Typography} from "@mui/material";
-import {Link as RouterLink} from "react-router-dom";
+import {
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
 import IconButton from "@mui/material/IconButton";
-import {Delete, Favorite, FavoriteBorder} from "@mui/icons-material";
-import React, {useEffect, useState} from "react";
-import {useTranslation} from "react-i18next";
-import {useDispatch, useSelector} from "react-redux";
+import { Delete, Favorite, FavoriteBorder } from "@mui/icons-material";
+import React, { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useDispatch, useSelector } from "react-redux";
 import Box from "@mui/material/Box";
-import {setFavorite} from "../Store/Action/FavouriteAction";
+import { setFavorite } from "../Store/Action/FavouriteAction";
+import AlertDialog from "./AlertDialogComp";
 
 function CourseComp(props) {
-    const [t, i18n] = useTranslation("global");
-    const [isFavorite, setIsFavorite] = useState(false);
-    const favorites = useSelector(state => state.favorites.favorites);
-    const dispatch = useDispatch();
+  const [t, i18n] = useTranslation("global");
+  const [isFavorite, setIsFavorite] = useState(false);
+  const favorites = useSelector((state) => state.favorites.favorites);
+  const dispatch = useDispatch();
+  const [openDialog, setOpenDialog] = useState(false);
 
-    const CheckFavorite = () => {
-        favorites.map((course) => {
-            if (course.id === props.course.id) {
-                setIsFavorite(true);
-                return true;
-            }
-            return false;
-        });
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  };
+
+  const CheckFavorite = () => {
+    favorites.map((course) => {
+      if (course.id === props.course.id) {
+        setIsFavorite(true);
+        return true;
+      }
+      return false;
+    });
+  };
+
+  const ToggleFavorites = (e) => {
+    e.preventDefault();
+    if (isFavorite) {
+      setIsFavorite(false);
+      let newFavorite = favorites.filter(
+        (course) => course.id !== props.course.id
+      );
+      localStorage.setItem("favorites", JSON.stringify(newFavorite));
+      dispatch(setFavorite(newFavorite));
+    } else {
+      setIsFavorite(true);
+      favorites.push(props.course);
+      localStorage.setItem("favorites", JSON.stringify(favorites));
+      dispatch(setFavorite(favorites));
     }
+    setIsFavorite(!isFavorite);
+  };
 
-    const ToggleFavorites = (e) => {
-        e.preventDefault();
-        if (isFavorite) {
-            setIsFavorite(false);
-            let newFavorite = favorites.filter((course) => course.id !== props.course.id);
-            localStorage.setItem("favorites", JSON.stringify(newFavorite));
-            dispatch(setFavorite(newFavorite));
-        } else {
-            setIsFavorite(true);
-            favorites.push(props.course);
-            localStorage.setItem("favorites", JSON.stringify(favorites));
-            dispatch(setFavorite(favorites));
-        }
-        setIsFavorite(!isFavorite);
-    }
+  const DeleteFavorite = (e) => {
+    e.preventDefault();
+    favorites.splice(props.index, 1);
 
-    const DeleteFavorite = (e) => {
-        e.preventDefault();
-        favorites.splice(props.index, 1);
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+    dispatch(setFavorite(favorites));
+  };
 
-        localStorage.setItem("favorites", JSON.stringify(favorites));
-        dispatch(setFavorite(favorites));
-    }
+  useEffect(() => {
+    CheckFavorite();
+  }, [props.course]);
 
-    useEffect(() => {
-        CheckFavorite();
-    }, [props.course]);
+  return (
+    <>
+      <Card>
+        <CardMedia
+          component="img"
+          image={props.course.image}
+          alt={i18n.language === "en" ? props.course.name : props.course.nameAr}
+          sx={{
+            height: "20vh",
+            width: "100%",
+          }}
+        />
+        <CardContent>
+          <Typography variant="h6" component="div">
+            {props.course.name.length > 12
+              ? `${
+                  i18n.language === "en"
+                    ? props.course.name.substring(0, 12)
+                    : props.course.nameAr.substring(0, 12)
+                }...`
+              : i18n.language === "en"
+              ? props.course.name
+              : props.course.nameAr}
+          </Typography>
+          <Typography variant="body1" color="textSecondary">
+            ${props.course.price}
+          </Typography>
 
-
-    return (
-        <Card>
-            <CardMedia
-                component="img"
-                image={props.course.image}
-                alt={i18n.language === "en" ? props.course.name : props.course.nameAr}
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              alignContent: "center",
+            }}
+          >
+            <Button
+              variant="outlined"
+              color="primary"
+              component={RouterLink}
+              to={`/courses/${props.course.id}`}
+              sx={{
+                width: "70%",
+              }}
+            >
+              {t("home.goToCourse")}
+            </Button>
+            {props.favPage ? (
+              <IconButton
+                onClick={() => setOpenDialog(true)}
                 sx={{
-                    height: "20vh",
-                    width: "100%"
+                  color: "red",
+                  marginLeft: "2vw",
+                  width: "auto",
+                  height: "auto",
                 }}
-            />
-            <CardContent>
-                <Typography variant="h6" component="div">
-                    {props.course.name.length > 12
-                        ? `${i18n.language === "en" ? props.course.name.substring(0, 12) : props.course.nameAr.substring(0, 12)}...`
-                        : i18n.language === "en" ? props.course.name : props.course.nameAr}
-                </Typography>
-                <Typography variant="body1" color="textSecondary">
-                    ${props.course.price}
-                </Typography>
-
-                <Box sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    alignContent: "center",
-                }}>
-                    <Button variant="outlined" color="primary" component={RouterLink}
-                            to={`/courses/${props.course.id}`} sx={{
-                        width:"70%"
-                    }}>
-                        {t("home.goToCourse")}
-                    </Button>
-                    { props.favPage ? (
-                        <IconButton
-                            onClick={DeleteFavorite}
-                            sx={{
-                                color: "red",
-                                marginLeft: "2vw",
-                                width: "auto",
-                                height: "auto",
-
-                            }}>
-                            <Delete sx={{
-                                fontSize: "2rem"
-                            }}/>
-                        </IconButton>
-                    ) : (<IconButton onClick={ToggleFavorites}
-                                     sx={{
-                                         color: isFavorite ? "red" : "gray",
-                                         marginLeft: "1vw",
-                                         width: "auto",
-                                         height: "auto",
-                                     }}>
-                        {isFavorite ? <Favorite
-                            sx={{
-                                fontSize: "2rem"
-                            }}/> : <FavoriteBorder
-                            sx={{
-                                fontSize: "2rem"
-                            }}/>}
-                    </IconButton>)}
-                </Box>
-
-            </CardContent>
-        </Card>
-    )
+              >
+                <Delete
+                  sx={{
+                    fontSize: "2rem",
+                  }}
+                />
+              </IconButton>
+            ) : (
+              <IconButton
+                onClick={ToggleFavorites}
+                sx={{
+                  color: isFavorite ? "red" : "gray",
+                  marginLeft: "1vw",
+                  width: "auto",
+                  height: "auto",
+                }}
+              >
+                {isFavorite ? (
+                  <Favorite
+                    sx={{
+                      fontSize: "2rem",
+                    }}
+                  />
+                ) : (
+                  <FavoriteBorder
+                    sx={{
+                      fontSize: "2rem",
+                    }}
+                  />
+                )}
+              </IconButton>
+            )}
+          </Box>
+        </CardContent>
+      </Card>
+      <AlertDialog
+        content={t("wishlist.dialogContent")}
+        title={t("wishlist.dialogTitle")}
+        openDialog={openDialog}
+        handleCloseDialog={handleCloseDialog}
+        confirm={DeleteFavorite}
+      />
+    </>
+  );
 }
 
-export default CourseComp
+export default CourseComp;
--- react_e_learning_project/src/Pages/WishListPage.jsx
+++ react_e_learning_project/src/Pages/WishListPage.jsx
@@ -1,54 +1,65 @@
 import Box from "@mui/material/Box";
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 import CourseComp from "../Components/CourseComp";
-import {Container, Grid} from "@mui/material";
-import {useEffect, useState} from "react";
-import {useTranslation} from "react-i18next";
+import { Container, Grid } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
 
 function WishListPage() {
-    const state = useSelector(state => state);
-    const favoritesState = state.favorites.favorites;
-    const [favorites, setFavorites] = useState([]);
-    const [t, i18n] = useTranslation("global");
+  const state = useSelector((state) => state);
+  const favoritesState = state.favorites.favorites;
+  const [favorites, setFavorites] = useState([]);
+  const [t, i18n] = useTranslation("global");
 
-    useEffect(() => {
-        setFavorites(favoritesState);
-    }, [favoritesState]);
+  useEffect(() => {
+    setFavorites(favoritesState);
+  }, [favoritesState]);
 
-
-
-    return (<Container sx={{
-            marginTop: "5vh", padding: "10px",
-        }}>
-            {favorites.length === 0 && (<Box sx={{
-                    textAlign: "center",
-                    color: "red",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    alignContent: "center",
-                    height: "68vh",
-                }}>
-                    <h1>{t("wishlist.noCourses")}</h1>
-                </Box>)
-
-            }
-            <Grid container rowSpacing={10} columnSpacing={{xs: 1, sm: 2, md: 3}} alignItems={"center"}
-                  alignContent={"center"}>
-                {favorites.map((course, index) => (<Grid item xs={12} sm={6} md={4} key={course.id}>
-                        <CourseComp course={course} favPage={true} index={index}/>
-                    </Grid>))}
-            </Grid>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-
-        </Container>);
+  return (
+    <Container
+      sx={{
+        marginTop: "5vh",
+        padding: "10px",
+      }}
+    >
+      {favorites.length === 0 && (
+        <Box
+          sx={{
+            textAlign: "center",
+            color: "red",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            alignContent: "center",
+            height: "68vh",
+          }}
+        >
+          <h1>{t("wishlist.noCourses")}</h1>
+        </Box>
+      )}
+      <Grid
+        container
+        rowSpacing={10}
+        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+        alignItems={"center"}
+        alignContent={"center"}
+      >
+        {favorites.map((course, index) => (
+          <Grid item xs={12} sm={6} md={4} key={course.id}>
+            <CourseComp course={course} favPage={true} index={index} />
+          </Grid>
+        ))}
+      </Grid>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </Container>
+  );
 }
 
 export default WishListPage;
